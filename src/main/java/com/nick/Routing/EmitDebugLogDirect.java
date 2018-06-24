ebee413a3b44fd05308b71ec2f665b7d58570e68
qiujiahong <qiujh@szfangle.com>
2018-06-24 09:59:09 +0800

    添加路由模式；

---
+++ src/main/java/com/nick/Routing/EmitDebugLogDirect.java
@@ -0,0 +1,32 @@
+package com.nick.Routing;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.util.concurrent.TimeoutException;
+
+public class EmitDebugLogDirect {
+
+    private static final String EXCHANGE_NAME = "direct_logs";
+
+    public static void main(String[] argv)
+            throws java.io.IOException, TimeoutException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("192.168.5.136");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+
+        String severity = "DEBUG";
+        String message = "debug message";
+
+        channel.basicPublish(EXCHANGE_NAME, severity, null, message.getBytes());
+        System.out.println(" [x] Sent '" + severity + "':'" + message + "'");
+
+        channel.close();
+        connection.close();
+    }
+}
