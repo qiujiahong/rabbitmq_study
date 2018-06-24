07dfdf933846668ebf3d657004eedb4c13b90236
qiujiahong <qiujh@szfangle.com>
2018-06-24 09:31:05 +0800

    工作队列模式；

---
+++ src/main/java/com/nick/workQueues/Send.java
@@ -0,0 +1,33 @@
+package com.nick.workQueues;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+import java.util.concurrent.TimeoutException;
+
+public class Send {
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] argv)
+            throws java.io.IOException, TimeoutException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("192.168.5.136");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
+        String message = "hello work queues";
+
+        channel.basicPublish( "", TASK_QUEUE_NAME,
+                MessageProperties.PERSISTENT_TEXT_PLAIN,
+                message.getBytes());
+        System.out.println(" [x] Sent '" + message + "'");
+
+        channel.close();
+        connection.close();
+    }
+}
